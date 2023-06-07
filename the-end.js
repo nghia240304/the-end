a166a3ba0bed61a2db69a2bfcb134de540360214
nghia240304 <hnghialop91dhp@gmail.com>
2023-06-07 10:39:32 +0700

    the end

---
+++ the-end.js
@@ -0,0 +1,17 @@
+//toTop//
+let toTop = document.getElementById('toTop');
+toTop.style.display = 'none';
+window.addEventListener('scroll', () => {
+    if(this.scrollY > 300){
+        toTop.style.display = 'block';
+    }else{
+        toTop.style.display = 'none';
+    }
+})
+
+toTop.onclick = function (){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
